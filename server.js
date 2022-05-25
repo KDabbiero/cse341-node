5100c3c28863c4e708b257ecca897d63c50a3079
KDabbiero <katelyndabbiero@gmail.com>
2022-05-24 19:10:11 -0600

    Week04

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const connectDB = require('./db/connection');
 const app = express();
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 connectDB();
 
@@ -9,3 +13,17 @@ app.use('/api/contactModel', require('./api/user'))
 const Port = process.env.Port || 3000;
 
 app.listen(Port, () => console.log('Server started'));
+
+app
+    .use(bodyParser.json())
+    .use((req, res, next) => {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader(
+            'Access-Control-Allow-Headers',
+            'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
+        );
+        res.setHeader('Content-Type', 'application/json');
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        next();
+    })
+    .use('/', require('./routes'));
